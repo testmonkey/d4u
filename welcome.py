dbb2ee2a49e2195a5bba3b7c94e0c2409a38fa7f
testmonkey <pethe.ranjeet@gmail.com>
2020-08-24 23:25:09 +1000

    welcome flow added

---
+++ welcome.py
@@ -0,0 +1,33 @@
+def chooseUserProfile():
+		userProfile = input("Press 'D' for driver or 'M' for merchant: \n")
+		
+		# print(userProfile)
+		
+		if userProfile != 'D' and userProfile != 'M' :
+			print("Please use correct option")
+
+		else:
+
+			if userProfile =='D':
+				print('Welcome to our driver registration app')
+				
+			elif userProfile =='M':
+				print('Welcome to our merchant registration app')
+		return(userProfile)	
+
+def signUporSignIn():
+		chooseOption = input("Please select your option SignUp (1) or SignIn (2) :\n")
+
+
+		if chooseOption != '1' and chooseOption != '2' :
+			print("Sorry please choose correct option")
+		else:
+			if chooseOption == '1':
+				print('please enter your username :\n')
+				
+			elif chooseOption =='2':
+				print('welcome to registration page')
+
+
+selectedprofile = chooseUserProfile()
+signUporSignIn()
